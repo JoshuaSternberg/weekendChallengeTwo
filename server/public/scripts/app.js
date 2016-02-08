08d7acd6e1dc67cf9cd80d88ffc47c066ef318ab
Joshua Sternberg <JoshuaSternberg@Joshuas-MacBook-Air.local>
2016-02-08 17:09:47 -0600

    added picture functionality

--- server/public/scripts/app.js
+++ server/public/scripts/app.js
@@ -1,16 +1,10 @@
 var peopleArray = [];
 var currentPerson = 1;
 
-
 $(document).ready(function(){
     getData();
-    //changePerson(currentPerson);
-
-    //console.log(peopleArray);
     
-    //changePerson(currentPerson);        
     $('button').on('click', buttonClick);
-
 });
 
 function getData(){
@@ -19,7 +13,8 @@ function getData(){
         url:"/data",
         success: function(data) {
             peopleArray = data.people; 
-            //console.log(peopleArray);
+
+            updateDom();
         },
         error: function() {
             console.log('ERROR: Unable to contact the server.');
@@ -29,45 +24,43 @@ function getData(){
 }
 
 function buttonClick () {
-    $('button').removeClass('selected');
+    removeClass();
 
-        var currentButton = $(this).attr('class');
-        console.log(currentButton);
-        if (currentButton != 'next' && currentButton != 'previous'){
-            currentPerson = $(this).attr('class');
-             $(this).addClass('selected');
-            changePerson(currentPerson);
-        } else if (currentButton == 'next') {
-            if (currentPerson == 20) {
-                currentPerson = 1;
-                changePerson(currentPerson);
-                $('.' + currentPerson).addClass('selected');
-            } else {
-                currentPerson++;
-                changePerson(currentPerson);
-                $('.' + currentPerson).addClass('selected');
-            }
-        } else if (currentButton == 'previous') {
-            if (currentPerson == 1) {
-                currentPerson = 20;
-                changePerson(currentPerson);
-                $('.' + currentPerson).addClass('selected');
-            } else {
-                currentPerson--;
-                changePerson(currentPerson);
-                $('.' + currentPerson).addClass('selected');
-            }
+    var currentButton = $(this).attr('class');
+    if (currentButton != 'next' && currentButton != 'previous'){
+        currentPerson = $(this).attr('class');
+        $(this).addClass('selected');
+    } else if (currentButton == 'next') {
+        if (currentPerson == 20) {
+            currentPerson = 1;
+        } else {
+            currentPerson++;
         }
+    } else if (currentButton == 'previous') {
+        if (currentPerson == 1) {
+            currentPerson = 20;
+        } else {
+            currentPerson--;
+        }
+    }
+    updateDom(); 
 }
 
 function changePerson (person){
-        //$('#people').addClass('hide');
         $('#people').fadeOut(0);
+        $('img').attr('src', peopleArray[person-1].picture);
         $('.name').text(peopleArray[person-1].name);
         $('.movie1').text(peopleArray[person-1].favoriteMovie1);
         $('.movie2').text(peopleArray[person-1].favoriteMovie2);
         $('.song').text(peopleArray[person-1].favoriteSong);
-        //$('#people').removeClass('hide');
         $('#people').fadeIn(500);
 }
 
+function removeClass (){
+    $('button').removeClass('selected');
+}
+
+function updateDom (){
+    changePerson(currentPerson);
+    $('.' + currentPerson).addClass('selected');
+}
